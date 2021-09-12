523b01beba98ce580e251241357e0c6579c4c355
Skarulis Justinas <justinas.skarulis@mif.stud.vu.lt>
2021-09-13 01:52:42 +0300

    Added test class

---
+++ test/com/my/validation/PasswordCheckerTest.java
@@ -0,0 +1,14 @@
+package com.my.validation;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PasswordCheckerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
