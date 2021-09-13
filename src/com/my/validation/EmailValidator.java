32ebea48de95084e29a158f61fa4a95de58a6552
Skarulis Justinas <justinas.skarulis@mif.stud.vu.lt>
2021-09-13 12:54:08 +0300

    UnitTests

---
+++ src/com/my/validation/EmailValidator.java
@@ -0,0 +1,20 @@
+package com.my.validation;
+
+public class EmailValidator {
+
+	public boolean checkEmailSymbol(String email) {
+		return true;
+	}
+
+	public boolean checkEmail(String email) {
+		return true;
+	}
+
+	public boolean checkDomain(String email) {
+		return true;
+	}
+
+	public boolean checkTLD(String email) {
+		return true;
+	}
+}
---
+++ src/com/my/validation/PhoneValidator.java
@@ -0,0 +1,15 @@
+package com.my.validation;
+
+public class PhoneValidator {
+
+	public boolean checkPhoneNumber(String phoneNumber) {
+		return false;
+	}
+
+	public String checkFirstNumber(String phoneNumber) {
+		return phoneNumber;
+	}
+
+	public void addPhonePrefix(String length, String prefix) {
+	}
+}
