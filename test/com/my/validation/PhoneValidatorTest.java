2af2b2e92604049b70acc4fc47e0d6fb00a57c10
Skarulis Justinas <justinas.skarulis@mif.stud.vu.lt>
2021-09-13 12:59:14 +0300

    Unit Tests

---
+++ test/com/my/validation/PhoneValidatorTest.java
@@ -0,0 +1,32 @@
+package com.my.validation;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class PhoneValidatorTest {
+
+	PhoneValidator phoneValidator = new PhoneValidator();
+
+	@Test
+	void checkPhoneNumberTestSuccess() {
+		assertTrue(phoneValidator.checkPhoneNumber("+37062847379"));
+	}
+
+	@Test
+	void checkPhoneNumberTestFail() {
+		assertFalse(phoneValidator.checkPhoneNumber("+3706284737R"));
+	}
+
+	// tikrina, kai reikia pakeisti is 8 i +370
+	@Test
+	void checkFirstNumberChangeTest() {
+		assertEquals("+37062847379", phoneValidator.checkFirstNumber("862847379"));
+	}
+
+	// tikrina, kai nereikia pakeisti is 8 i +370
+	@Test
+	void checkFirstNumberNoChangeTest() {
+		assertEquals("+37062847379", phoneValidator.checkFirstNumber("+37062847379"));
+	}
+}
