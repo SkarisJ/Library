ffcbfd31c9571b31153bc1579ae28641e77342b9
Skarulis Justinas <justinas.skarulis@mif.stud.vu.lt>
2021-09-13 11:41:59 +0300

    Added new class + coding

--- src/com/my/validation/PasswordChecker.java
+++ src/com/my/validation/PasswordChecker.java
@@ -2,4 +2,17 @@ package com.my.validation;
 
 public class PasswordChecker {
 
+	public boolean checkLength(String password) {
+		
+		return true;
+	}
+	
+	public boolean checkUpperCase(String password) {
+		return true;
+	}
+	
+	public boolean checkSpecialSymbol(String password) {
+		return true;
+	}
+	
 }
--- src/com/my/validation/PhoneValidator.java
+++ src/com/my/validation/PhoneValidator.java
@@ -2,4 +2,16 @@ package com.my.validation;
 
 public class PhoneValidator {
 
+	public boolean checkPhoneNumber(String phoneNumber) {
+		return true;
+	}
+	
+	public boolean checkFirstNumber(String phoneNumber) {
+		return true;
+	}
+	
+	public String adjustPhoneNumber(String phoneNumber, String length, String prefix) {
+		
+		return phoneNumber;
+	}
 }
